0958b42f87bcf06d95d6463ac1d1e1f26e212fac
fran <francisco.vento@outlook.com>
2021-07-12 19:49:21 -0700

    ejercicio de clase

--- first-aplication/src/App.js
+++ first-aplication/src/App.js
@@ -1,29 +1,57 @@
 import './App.css'
-import { useEffect, useState } from 'react'
-import searchGif from './services/searchGif'
-import Gif from './components/Gif'
-import Search from './components/Search'
+import { useForm } from 'react-hook-form'
 
 function App () {
-  const [currentGif, setCurrentGif] = useState({
-    images: {
-      original: {
-        url: ''
-      }
-    }
-  })
-  const [queryTerm, setQueryTerm] = useState('puppies')
-  useEffect(() => {
-    searchGif(queryTerm).then(data => {
-      setCurrentGif(data.data[0])
-    })
-  }, [queryTerm])
+ const { handleSubmit, register } = useForm()
 
+const callBack = (formValues) =>{
+  let arr = JSON.stringify(formValues)
+  alert(arr)  
+} 
+
+const callBackErrores = (errores) =>{
+  console.log(errores)
+}
   return (
     <div className='App'>
       <header className='App-header'>
-        <Search changeQuery={setQueryTerm} />
-        <Gif url={currentGif.images.original.url} />
+        <form onSubmit={handleSubmit(callBack, callBackErrores)}>
+          <div>
+            <label style={{display: 'flex', flexDirection: 'column' }}>
+              Nombre 
+              <input type="text" { ...register('nombre') } />
+            </label>
+          </div>
+          <div>
+            <label style={{display: 'flex', flexDirection: 'column' }}>
+              Apellido: 
+              <input type="text" { ...register('apellido', {required: true})} />
+            </label>
+          </div>
+          <div>
+            <label style={{display: 'flex', flexDirection: 'column' }}>
+              Edad: 
+              <input type="number" { ...register('edad', {required: true})} />
+            </label>
+          </div>
+          <div>
+            <label style={{display: 'flex', flexDirection: 'column' }}>
+              Correo: 
+              <input type="email" { ...register('email', {required: true})} />
+            </label>
+          </div>
+          <div>
+            <label style={{display: 'flex', flexDirection: 'column' }}>
+              Pais: 
+              <input type="text" { ...register('pais', {required: true})} />
+            </label>
+          </div>
+          <button type='submit'> Enviar </button>
+
+        </form>
+
+
+
       </header>
     </div>
   )
